b90b4fd219466edf36ca3f218905b025d31d25f2
SimonWu <wuhanyu@gmail.com>
2013-08-28 09:59:34 +0800

    init

---
+++ nobodyhere/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nobodyhere::Application.config.secret_token = 'c105dcd500b3fef8e9e558490b4dedc922007b0084748b73689b51bb785494ef24c853a07fab3da92541c3ffdff05278aeab8765a48fdfd4914d9592960badaf'
